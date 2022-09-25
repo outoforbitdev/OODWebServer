f05721131f704fe20a7613010d038140540b5412
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2022-09-25 08:40:08 -0500

    Copy Account client application

---
+++ OODWebServer/OODWebServer/ClientApp/src/Applications/Account/Components/Creation.tsx
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import { Screens } from '../App';
+import { Button } from '../core/Components/Button';
+import { Link } from '../core/Components/Link';
+import { Modal } from '../core/Components/Modal';
+import { PasswordField } from '../core/Components/PasswordField';
+import { TextField } from '../core/Components/TextField';
+import { ILoginProps } from './Login';
+
+export function Creation(props: ILoginProps) {
+    return (
+        <Modal>
+            <label>Email</label>
+            <div><TextField clearable /></div>
+            <div><span /></div>
+            <label>Password</label>
+            <div><PasswordField showable /></div>
+            <label>Confirm Password</label>
+            <div><PasswordField showable /></div>
+            <Button text={"Create Account"}
+                width={"234px"}
+            /><br />
+            <Link onClick={() => props.changeScreen(Screens.Login)}>
+                I have an existing account
+            </Link>
+        </Modal>
+    );
+}
---
+++ OODWebServer/OODWebServer/ClientApp/src/Applications/Account/Components/Login.tsx
@@ -0,0 +1,30 @@
+﻿import * as React from 'react';
+import { Screens } from '../App';
+import { Button } from '../core/Components/Button';
+import { IComponentProps } from '../core/Components/IComponentProps';
+import { Link } from '../core/Components/Link';
+import { Modal } from '../core/Components/Modal';
+import { PasswordField } from '../core/Components/PasswordField';
+import { TextField } from '../core/Components/TextField';
+
+export interface ILoginProps extends IComponentProps {
+    changeScreen: (screen: Screens) => void;
+}
+
+export function Login(props: ILoginProps) {
+    return (
+        <Modal>
+            <label>Username</label>
+            <div><TextField clearable /></div>
+            <div><span /></div>
+            <label>Password</label>
+            <div><PasswordField showable /></div>
+            <Button text={"Login"}
+                width={"234px"} 
+            /><br />
+            <Link onClick={() => props.changeScreen(Screens.Create)}>
+                Create Account
+            </Link>
+        </Modal>
+    );
+}
