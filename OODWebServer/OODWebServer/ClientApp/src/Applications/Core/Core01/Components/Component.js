667380dc0d4f1e0ec9e62aff48844c1f77286356
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2022-10-13 19:28:12 -0500

    Remove .js and .js.map

--- OODWebServer/OODWebServer/ClientApp/src/Applications/Core/Core01/Components/Component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UniqueKey = void 0;
-var uuid_1 = require("uuid");
-function UniqueKey(componentName) {
-    return componentName + (0, uuid_1.v4)();
-}
-exports.UniqueKey = UniqueKey;
-//# sourceMappingURL=Component.js.map
